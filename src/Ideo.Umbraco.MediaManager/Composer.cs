3b9e35242fda4fa9bd1eb4c86302b45ce2369113
Mykhailo Hrinin <mykhailo.hrinin@ideo-digital.com>
2026-07-01 14:50:30 +0300

    Remove unneeded application-part registration; controllers auto-discover (matches contentsearch)

--- src/Ideo.Umbraco.MediaManager/Composer.cs
+++ src/Ideo.Umbraco.MediaManager/Composer.cs
@@ -1,6 +1,5 @@
 using Ideo.Umbraco.MediaManager.Interfaces;
 using Ideo.Umbraco.MediaManager.Services;
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.DependencyInjection;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
@@ -17,25 +16,5 @@ public class Composer : IComposer
         builder.Services.AddSingleton<ScanJobManager>();
         builder.Services.AddSingleton<IScanJobManager>(provider => provider.GetRequiredService<ScanJobManager>());
         builder.Services.AddHostedService(provider => provider.GetRequiredService<ScanJobManager>());
-
-        RegisterControllers(builder);
-    }
-
-    // Ensure this assembly's API controllers are discovered as an MVC application part. When the
-    // package is consumed as a project reference the part is not auto-added; guard against
-    // double-registration for the case where it already is (e.g. installed as a NuGet package).
-    private static void RegisterControllers(IUmbracoBuilder builder)
-    {
-        var assembly = typeof(Composer).Assembly;
-        var mvcBuilder = builder.Services.AddControllers();
-
-        var alreadyRegistered = mvcBuilder.PartManager.ApplicationParts
-            .OfType<AssemblyPart>()
-            .Any(part => part.Assembly == assembly);
-
-        if (!alreadyRegistered)
-        {
-            mvcBuilder.AddApplicationPart(assembly);
-        }
     }
 }
